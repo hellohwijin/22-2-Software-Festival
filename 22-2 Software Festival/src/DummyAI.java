41ddc9d5674e48fa62469e99076b400f3ee46135
skqhfla <83075908+skqhfla@users.noreply.github.com>
2022-08-10 19:49:29 +0900

    Update DummyAI.java

--- 22-2 Software Festival/src/DummyAI.java
+++ 22-2 Software Festival/src/DummyAI.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 
 public class DummyAI {
+	
+	static int mine;
+	static int opponent;
+	
+	
+	//main method
 	public static void main(String[] args) throws Exception {
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Input the ip address > ");
 		String ip = scanner.nextLine();
@@ -10,9 +17,21 @@ public class DummyAI {
 		System.out.print("Input the color > ");
 		String color = scanner.nextLine();
 		
-		ConnectSix conSix = new ConnectSix(ip, port, color);
-		System.out.println("Red Stone positions are " + conSix.redStones);
+		//베타고에서 사용하기 위한 머시깽이... ----------------------------------------
 
+		if (color.toLowerCase().compareTo("white") == 0) {
+			mine = 2;
+			opponent = 1;
+		} else if (color.toLowerCase().compareTo("black") == 0) {
+			mine = 1;
+			opponent = 2;
+		} 
+		//----------------------------------------------------------------
+		
+		ConnectSix conSix = new ConnectSix(ip, port, color); //making instance of ConnectSix as name conSix
+		System.out.println("Red Stone positions are " + conSix.redStones);
+		
+		//if AI choose black, start with one stone at the middle of the board.
 		if (color.toLowerCase().compareTo("black") == 0) {
 			String first = conSix.drawAndRead("K10");
 		} else if (color.toLowerCase().compareTo("white") == 0) {
@@ -20,12 +39,17 @@ public class DummyAI {
 		}
 		
 		while (true) {
+			String draw;
+			
+			/*
 			char alpha1 = (char) ((Math.random() * 19) + 'A');
 			int num1 = (int)( Math.random() * 19) + 1;
 			char alpha2 = (char) ((Math.random() * 19) + 'A');
 			int num2 = (int)( Math.random() * 19) + 1;
+			draw = String.format("%c%02d:%c%02d", alpha1, num1, alpha2, num2);
+			*/
 			
-			String draw = String.format("%c%02d:%c%02d", alpha1, num1, alpha2, num2);
+			draw = Betago.returnStringCoor();
 			
 			String read = conSix.drawAndRead(draw);
 			
@@ -35,4 +59,43 @@ public class DummyAI {
 		}
 
 	}
+	
+	// return the AI color. black = 1, white = 2, red = 3, empty = 0
+	public static int getMyColor() {
+		return mine;
+	}
+	// return the person color. black = 1, white = 2, red = 3, empty = 0
+		public static int getYourColor() {
+			return opponent;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
